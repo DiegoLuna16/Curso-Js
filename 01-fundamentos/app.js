7d50d2b205f20920b196167a8a9e89220f1ceed2
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-02 17:55:48 -0600

    operaciones con consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -4,4 +4,18 @@
 // var b = 10;
 // const c = 10;
 
-let a = 10, b = 20, x = a + b;
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman'
+    x = a + b;
+
+
+const saludo = c + d;
+
+console.log(saludo)
+console.log({a}) 
+console.log({b}) 
+console.log({x}) 
+
+// console.table({a,b,c,d,x})
