e53bcdf2e2f7a3e81b584e8648a76cc9038d706e
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-06 18:06:26 -0600

    setting up project

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,24 @@
+
+let nombre = 'Peter'
+console.log(nombre)
+
+nombre = 'Diego'
+console.log(nombre)
+
+console.log( typeof nombre)
+
+nombre = 123
+
+console.log( typeof nombre)
+
+let esMarvel = true
+console.log(typeof esMarvel)
+
+let edad = 33
+console.log(typeof edad)
+
+const persona = {
+    name : 'Diego',
+    lastname: 'Luna'
+} 
+
