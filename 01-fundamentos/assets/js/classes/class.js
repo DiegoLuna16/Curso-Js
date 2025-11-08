6f6d348a8504431e6e2129e0f7cee723b962010d
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-08 13:16:38 -0600

    creating a class

---
+++ 01-fundamentos/assets/js/classes/class.js
@@ -0,0 +1,15 @@
+class Persona {
+
+    nombre = '';
+    codigo = '';
+    frase = '';
+    
+    constructor(nombre,codigo,frase){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
+    }
+}
+
+const diego = new Persona('Diego', 'Luna', 'Helloo');
+console.log(diego);
