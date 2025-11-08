a835450503afea23232476150b963bd60258f4ca
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-08 13:38:46 -0600

    static properties and methods

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,4 +1,14 @@
 class Persona {
+  static _conteo = 0;
+
+    static get conteo() {
+        return Persona._conteo;
+    }
+
+    static mensaje() {
+        console.log('Hola a todos soy un metodo estatico');
+    }
+
   nombre = "";
   codigo = "";
   frase = "";
@@ -8,16 +18,16 @@ class Persona {
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+    Persona._conteo++;
   }
 
-  set setComidaFavorita (comida) {
-    this.comida = comida
+  set setComidaFavorita(comida) {
+    this.comida = comida;
   }
 
   get getComidaFavorita() {
-    return this.comida
+    return this.comida;
   }
-
 
   quienSoy() {
     console.log(`Soy ${this.nombre}`);
@@ -27,6 +37,6 @@ class Persona {
 const diego = new Persona("Diego", "Luna", "Helloo");
 console.log(diego);
 diego.quienSoy();
-diego.setComidaFavorita = 'Bolas de Arroz'
+diego.setComidaFavorita = "Bolas de Arroz";
 console.log(diego.getComidaFavorita);
-
+console.log(Persona._conteo);
