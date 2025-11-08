dbc938641cd2fcc030ad61c6d4d62f3541c18df0
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-08 13:30:14 -0600

    set and get

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,20 +1,32 @@
 class Persona {
+  nombre = "";
+  codigo = "";
+  frase = "";
+  comida = "";
 
-    nombre = '';
-    codigo = '';
-    frase = '';
-    
-    constructor(nombre,codigo,frase){
-        this.nombre = nombre;
-        this.codigo = codigo;
-        this.frase = frase;
-    }
+  constructor(nombre, codigo, frase) {
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.frase = frase;
+  }
 
-    quienSoy() {
-        console.log(`Soy ${this.nombre}`);
-    }
+  set setComidaFavorita (comida) {
+    this.comida = comida
+  }
+
+  get getComidaFavorita() {
+    return this.comida
+  }
+
+
+  quienSoy() {
+    console.log(`Soy ${this.nombre}`);
+  }
 }
 
-const diego = new Persona('Diego', 'Luna', 'Helloo');
+const diego = new Persona("Diego", "Luna", "Helloo");
 console.log(diego);
-diego.quienSoy()
+diego.quienSoy();
+diego.setComidaFavorita = 'Bolas de Arroz'
+console.log(diego.getComidaFavorita);
+
