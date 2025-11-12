99aa9601245951947344d1e8ffd018b18e1c4376
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-11 19:05:35 -0600

    implementing store methods

--- 03-todo-app/src/store/todo.store.js
+++ 03-todo-app/src/store/todo.store.js
@@ -1,25 +1,83 @@
 import { Todo } from "../todos/models/todo.model";
 
 const Filters = {
-    All: 'all',
-    Completed: 'Completed',
-    Pending: 'Pending'
-}
+  All: "all",
+  Completed: "Completed",
+  Pending: "Pending",
+};
 
 const state = {
-    todos: [
-        new Todo('Prieda del alma'),
-        new Todo('Prieda del infinito'),
-        new Todo('Prieda del tiempo')
-    ],
-    filter: Filters.All
-}
+  todos: [
+    new Todo("Prieda del alma"),
+    new Todo("Prieda del infinito"),
+    new Todo("Prieda del tiempo"),
+    new Todo("Prieda de la luna"),
+    new Todo("Prieda de la realidad"),
+  ],
+  filter: Filters.All,
+};
 
 const initStore = () => {
-    console.log(state);
-    console.log('InitStore');
-}
+  console.log(state);
+  console.log("InitStore");
+};
+
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+const getTodos = (filter = Filters.All) => {
+    switch(filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done)
+        case Filters.Completed:
+            return state.todos.filter(todo => !todo.done)
+        default:
+            throw new Error(`Option ${filter} is not valid.`)
+    }
+};
+
+const addTodo = (description) => {
+  if (!description) throw new Error('Description is required')
+
+    state.todos.push(new Todo(description))
+};
+
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map(todo => {
+        if( todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+};
+
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId)
+};
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter(todo => todo.done)
+};
+
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+    return state.filter;
+};
 
 export default {
-    initStore,
-}
+  addTodo,
+  loadStore,
+  initStore,
+  toggleTodo,
+  getTodos,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
+};
