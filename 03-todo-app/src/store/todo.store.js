09863da9e1f260f28232d2e2fa3cea31e65acee0
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-13 17:05:29 -0600

    implementing local storage

--- 03-todo-app/src/store/todo.store.js
+++ 03-todo-app/src/store/todo.store.js
@@ -18,56 +18,68 @@ const state = {
 };
 
 const initStore = () => {
-  console.log(state);
+  loadStore()
   console.log("InitStore");
 };
 
 const loadStore = () => {
-  throw new Error("Not implemented");
+    if(!localStorage.getItem('state')) return;
+    const {todos = [], filter = Filters.All} = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
+};
+
+const saveStateToLocalStorage = () => {
+  localStorage.setItem("state", JSON.stringify(state));
 };
 
 const getTodos = (filter = Filters.All) => {
-    switch(filter) {
-        case Filters.All:
-            return [...state.todos];
-        case Filters.Completed:
-            return state.todos.filter(todo => todo.done)
-        case Filters.Completed:
-            return state.todos.filter(todo => !todo.done)
-        default:
-            throw new Error(`Option ${filter} is not valid.`)
-    }
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+    case Filters.Completed:
+      return state.todos.filter((todo) => todo.done);
+    case Filters.Completed:
+      return state.todos.filter((todo) => !todo.done);
+    default:
+      throw new Error(`Option ${filter} is not valid.`);
+  }
 };
 
 const addTodo = (description) => {
-  if (!description) throw new Error('Description is required')
+  if (!description) throw new Error("Description is required");
 
-    state.todos.push(new Todo(description))
+  state.todos.push(new Todo(description));
+  saveStateToLocalStorage();
 };
 
 const toggleTodo = (todoId) => {
-    state.todos = state.todos.map(todo => {
-        if( todo.id === todoId) {
-            todo.done = !todo.done;
-        }
-        return todo;
-    });
+  state.todos = state.todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+    }
+    return todo;
+  });
+  saveStateToLocalStorage();
 };
 
 const deleteTodo = (todoId) => {
-    state.todos = state.todos.filter(todo => todo.id !== todoId)
+  state.todos = state.todos.filter((todo) => todo.id !== todoId);
+  saveStateToLocalStorage();
 };
 
 const deleteCompleted = () => {
-    state.todos = state.todos.filter(todo => todo.done)
+  state.todos = state.todos.filter((todo) => todo.done);
+  saveStateToLocalStorage();
 };
 
 const setFilter = (newFilter = Filters.All) => {
-    state.filter = newFilter;
+  state.filter = newFilter;
+  saveStateToLocalStorage();
 };
 
 const getCurrentFilter = () => {
-    return state.filter;
+  return state.filter;
 };
 
 export default {
