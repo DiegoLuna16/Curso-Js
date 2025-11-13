ab8bcb485b5a7cfc97e0dab79e489ff05fce1809
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-12 21:53:07 -0600

    create function to display todos

--- 03-todo-app/src/todos/app.js
+++ 03-todo-app/src/todos/app.js
@@ -1,9 +1,24 @@
 import html from './app.html?raw'
+import todoStore from '../store/todo.store'
+import { renderTodos } from './use-cases/render-todos'
+
+
+const ElementIDs = {
+    TodoList: '.todo-list'
+}
 
 export const App = (elementId) => {
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(ElementIDs.TodoList, todos)
+    }
+
+
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html
         document.querySelector(elementId).append(app)
+        displayTodos()
     })();
 }
