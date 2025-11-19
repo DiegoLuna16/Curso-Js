8fcfd0e7fe1c8ac1f8793caab1edacf03614e5bb
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-18 20:51:21 -0600

    creating endpoint to retrieve random character

--- 05-http-app/src/breakingbad/breakingbad-app.js
+++ 05-http-app/src/breakingbad/breakingbad-app.js
@@ -1,10 +1,25 @@
+const fetchCharacter = async () => {
+  const value = getRandom1to12();
+  const res = await fetch(
+    "https://www.breakingbadapi.com/api/characters/" + value
+  );
+
+  const data = await res.json()
+  return data.name;
+};
+
 /**
- * 
- * @param {HTMLElement} element 
+ *
+ * @param {HTMLElement} element
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async(element) => {
+  document.querySelector("#app-title").innerHTML = "BreakingBad App";
+  element.innerHTML = "Loading...";
+  const data = await fetchCharacter()
+  console.log(data );
 
-    document.querySelector('#app-tile').innerHTML = 'BreakingBad App'
+};
 
-    element.innerHTML = 'Loading...'
-}
+const getRandom1to12 = () => {
+  return Math.floor(Math.random() * 12) + 1;
+};
