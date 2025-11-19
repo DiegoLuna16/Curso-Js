1ab1c08793b272da2a025f836473f2dcbc321a1d
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-11-18 20:23:59 -0600

    creating new project

---
+++ 05-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {HTMLElement} element 
+ */
+export const BreakingBadApp = (element) => {
+
+    document.querySelector('#app-tile').innerHTML = 'BreakingBad App'
+
+    element.innerHTML = 'Loading...'
+}
